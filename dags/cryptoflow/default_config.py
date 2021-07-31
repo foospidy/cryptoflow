a9669483cf6cc8a7f9119a369196f82e4efdcd2e
foospidy <phillip@foospidy.com>
2021-07-31 16:52:00 -0400

    added POLY

--- dags/cryptoflow/default_config.py
+++ dags/cryptoflow/default_config.py
@@ -686,6 +686,18 @@ DEFAULT_CF_CONFIG = {
             "schedule": None
         }
     },
+    "POLY": {
+        "btd": {
+            "amount_usd": 5,
+            "dip_price": 0.27,
+            "schedule": "*/60 * * * *",
+            "smallest_unit": 8
+        },
+        "dca": {
+            "amount_usd": 10,
+            "schedule": None
+        }
+    },
     "QNT": {
         "btd": {
             "amount_usd": 5,
