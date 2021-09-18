4315f6de7ac76fd4adf4873e4d4c8e8d1f4f2533
foospidy <phillip@foospidy.com>
2021-09-18 06:18:04 -0400

    add SLP

--- dags/cryptoflow/default_config.py
+++ dags/cryptoflow/default_config.py
@@ -878,6 +878,18 @@ DEFAULT_CF_CONFIG = {
             "schedule": None
         }
     },
+    "SLP": {
+        "btd": {
+            "amount_usd": 5,
+            "dip_price": 0.09,
+            "schedule": "*/60 * * * *",
+            "smallest_unit": 8
+        },
+        "dca": {
+            "amount_usd": 10,
+            "schedule": None
+        }
+    },
     "SNX": {
         "btd": {
             "amount_usd": 5,
