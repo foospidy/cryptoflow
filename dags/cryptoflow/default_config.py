2f09bde0214ea661d131db22800d00a7d018ee35
foospidy <phillip@foospidy.com>
2021-10-10 21:47:02 -0400

    added DDX

--- dags/cryptoflow/default_config.py
+++ dags/cryptoflow/default_config.py
@@ -326,6 +326,18 @@ DEFAULT_CF_CONFIG = {
             "schedule": None
         }
     },
+    "DDX": {
+        "btd": {
+            "amount_usd": 5,
+            "dip_price": 5.50,
+            "schedule": "*/60 * * * *",
+            "smallest_unit": 8
+        },
+        "dca": {
+            "amount_usd": 10,
+            "schedule": None
+        }
+    },
     "DNT": {
         "btd": {
             "amount_usd": 5,
