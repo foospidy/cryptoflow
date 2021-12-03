8fb2f41dab56d843f6a6545243a3682bd64363d9
foospidy <phillip@foospidy.com>
2021-12-03 06:28:14 -0500

    added BICO

--- dags/cryptoflow/default_config.py
+++ dags/cryptoflow/default_config.py
@@ -278,6 +278,18 @@ DEFAULT_CF_CONFIG = {
             "schedule": None
         }
     },
+    "BICO": {
+        "btd": {
+            "amount_usd": 5,
+            "dip_price": 8.10,
+            "schedule": "*/60 * * * *",
+            "smallest_unit": 8
+        },
+        "dca": {
+            "amount_usd": 10,
+            "schedule": None
+        }
+    },
     "BNT": {
         "btd": {
             "amount_usd": 5,
